b3ed6af764d5d9012730cbf8a8c68e697e2ad239
Daljit Singh <daljit7991@gmail.com>
2024-04-08 16:46:16 +0100

    Update toolchain file

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,4 +1,10 @@
-set(CMAKE_SYSTEM_PROCESSOR "arm")
-set(CMAKE_SYSTEM_NAME "Generic")
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_PROCESSOR arm64)
+SET(triple aarch64-linux-gnu)
+
+SET(CMAKE_C_COMPILER_TARGET ${triple})
+SET(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+SET(ENV{PKG_CONFIG_PATH} "/usr/lib/aarch64-linux-gnu/pkgconfig")
 
 
